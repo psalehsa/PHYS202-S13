a4186f743973bb3109d1de8fc1d6f4be83112bff
Parisa Salehsari <psalehsa@csm-pm-38123-101.local>
2013-05-07 16:00:58 -0700

    Midterm Turn in

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -7,3 +7,8 @@ def dipolePotential(x,y,q,d):
     k=8987551787
     Vxy=pointPotential(x,y,q,-d,0.)-pointPotential(x,y,q,d,0.)
     return Vxy
+def pointField(x,y,q,Xq,Yq):
+    k=8987551787
+    Ex_xy= (k*q)(x-Xq)/((((x-Xq)**(2.))+((y-Yd)**(2.)))**(1/2.))
+    Ey_xy=(k*q)(y-Yq)/((((x-Xq)**(2.))+((y-Yd)**(2.)))**(1/2.))
+    return Ex_xy,Ey_xy
